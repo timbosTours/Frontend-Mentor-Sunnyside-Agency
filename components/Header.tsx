75157b40d392d06da93336185c32aad2fa55be91
Timothy Fawcett <tim.fawcett89@gmail.com>
2023-01-04 21:24:46 +1100

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Header() {
     return (
         <>
-        <header className="bg-[url('/public/images/mobile/image-header.jpg')]" >
-            <h1 className="uppercase text-white">we are creatives</h1>
+            <header className="relative" >
+                <Image
+                    src={"/images/mobile/image-header.jpg"}
+                    alt={'header image'}
+                    width={500}
+                    height={500}
+                />
+                    <h1 className="uppercase text-white absolute top-1/4">we are creatives</h1>
             </header>
         </>
     )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar'
-import Header from '../components/header'
+import Header from '../components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
