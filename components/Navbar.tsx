f73e4e758cd5f6822cd0208d4ea4b5732ebfd1a0
Timothy Fawcett <tim.fawcett89@gmail.com>
2023-01-05 19:12:14 +1100

    added pages/refactored links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,34 +1,31 @@
 import * as React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 
 interface NavbarProps {
-    heading: string; 
-    onButton1Click: () => void;
-    onButton2Click: () => void;
-    onButton3Click: () => void;
-    onButton4Click: () => void;
+    heading: string;
 }
 
-const Navbar: React.FunctionComponent<NavbarProps> = ({ heading, onButton1Click, onButton2Click, onButton3Click, onButton4Click }) => {
+const Navbar: React.FunctionComponent<NavbarProps> = ({ heading }) => {
     return (
         <nav className="flex absolute items-center w-full justify-between px-4 py-3 ">
         <div className="flex items-center font-bold text-white mr-6">
         {heading}
         </div>
         <div className='hidden md:block'>
-        <button onClick={onButton1Click} className="px-4 py-2 font-bold text-white rounded-full hover:bg-transparent focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
+        <Link href="/About" className="px-4 py-2 font-bold text-white rounded-full hover:bg-transparent focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
             About
-        </button>
-        <button onClick={onButton2Click} className="px-4 py-2 font-bold text-white  rounded-full hover:bg-transparent focus:outline-none focus:shadow-outline-blue active:bg-blue-800 mr-4">
+        </Link>
+        <Link href="/Services" className="px-4 py-2 font-bold text-white  rounded-full hover:bg-transparent focus:outline-none focus:shadow-outline-blue active:bg-blue-800 mr-4">
             Services
-        </button>
-        <button onClick={onButton3Click} className="px-4 py-2 font-bold text-white  rounded-full hover:bg-transparent focus:outline-none focus:shadow-outline-blue active:bg-blue-800 mr-4">
+        </Link>
+        <Link href="Projects" className="px-4 py-2 font-bold text-white  rounded-full hover:bg-transparent focus:outline-none focus:shadow-outline-blue active:bg-blue-800 mr-4">
             Projects
-        </button>
-        <button onClick={onButton4Click} className="px-4 py-2 font-bold text-black bg-white rounded-full hover:bg-transparent focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
+        </Link>
+        <Link href="Contact" className="px-4 py-2 font-bold text-black bg-white rounded-full hover:bg-transparent focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
             Contact
-        </button>
+        </Link>
         </div>
                 <div className='sm:hidden' >
                     <Image
