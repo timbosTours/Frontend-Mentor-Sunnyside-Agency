8ed50518db02a7d6ca2166d18a6f342e697dce68
Timothy Fawcett <tim.fawcett89@gmail.com>
2023-01-13 18:57:17 +1100

    footer fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,36 +13,36 @@ function Footer() {
         <div className="bg-teal-200 h-full w-full text-center">
         <div className={barlow.className}>
             <h1 className="text-5xl text-teal-700 font-black m-20 mb-14 tracking-wide">sunnyside</h1>       
-            <ul className="text-teal-700 text-xl font-thin">
-                <Link className="m-12" href="/About">About</Link>
-                <Link className="m-12" href="/Services">Services</Link>
-                <Link className="m-12" href="/Projects">Projects</Link>
+            <ul className="text-teal-600 text-xl font-thin">
+                <Link className="m-5" href="/About">About</Link>
+                <Link className="m-5" href="/Services">Services</Link>
+                <Link className="m-5" href="/Projects">Projects</Link>
             </ul>
             </div>
             <div className="flex justify-center m-16">
                 <Image
-                className="m-6 mb-20 hover:cursor-pointer"
+                className="m-6 mb-20 hover:cursor-pointe"
                     src={"/images/icon-facebook.svg"}
                     alt="facebook"
                     width={30}
                     height={30}
                 />
                 <Image
-                className="m-6 mb-20 hover:cursor-pointer"
+                className="m-6 mb-20 hover:cursor-pointe"
                     src={"/images/icon-instagram.svg"}
                     alt="instagram"
                     width={30}
                     height={30}
                 />
                 <Image
-                className="m-6 mb-20 hover:cursor-pointer"
+                className="m-6 mb-20 hover:cursor-pointe"
                     src={"/images/icon-twitter.svg"}
                     alt="twitter"
                     width={30}
                     height={30}
                 />
                 <Image
-                className="m-6 mb-20 hover:cursor-pointer"
+                className="m-6 mb-20 hover:cursor-pointe"
                     src={"/images/icon-pinterest.svg"}
                     alt="pinterest"
                     width={30}
