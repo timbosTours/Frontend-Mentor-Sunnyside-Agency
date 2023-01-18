f04074e3248ef00e99f58bbabfe2b7ec9f5a7f02
Timothy Fawcett <tim.fawcett89@gmail.com>
2023-01-18 21:38:54 +1100

    grid

--- components/Content.tsx
+++ components/Content.tsx
@@ -20,6 +20,7 @@ const barlow = Barlow({
 function Content() {
     return (
         <>
+            <div className="md:grid md:grid-cols-2 md:grid-rows-3">
             <Image
             className="md:hidden"
             src='/images/mobile/image-transform.jpg'
@@ -27,11 +28,18 @@ function Content() {
             width={800}
             height={1000}
         />
-    <section className="h-96 w-96 text-center m-20 mb-1">
+            <Image
+            className=" max-md:hidden"
+            src='/images/desktop/image-transform.jpg'
+            alt="egg"
+            width={800}
+            height={1000}
+        />
+    <section className="h-96 text-center m-8 ">
             <div className={"text-gray-900 text-4xl weight"}>
             <h1 className={fraunces.className}>Transform your <br /> brand</h1>
             </div>
-            <p className="m-3 text-gray-500 text-xl">  We are a full-service creative agency specializing in helping brands grow fast. 
+            <p className="m-7 text-gray-500 text-xl">  We are a full-service creative agency specializing in helping brands grow fast. 
     Engage your clients through compelling visuals that do most of the marketing for you.</p>
             <div className={fraunces.className}>
             <Link
@@ -48,11 +56,18 @@ function Content() {
             width={800}
             height={1000}
             />
-            <section className="h-96 w-96 text-center m-20 mb-1">
+            <Image
+            className=" max-md:hidden"
+            src='/images/desktop/image-stand-out.jpg'
+            alt="cup"
+            width={800}
+            height={1000}
+            />
+            <section className="h-96 text-center m-10">
             <div className={"text-gray-900 text-4xl weight"}>
             <h1 className={fraunces.className}>Stand out too the <br /> right audience</h1>
             </div>
-            <p className="m-3 text-gray-500 text-xl"> Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places. </p>
+            <p className="m-4 text-gray-500 text-xl"> Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places. </p>
             <div className={fraunces.className}>
             <Link
                 className="text-xl"
@@ -61,10 +76,17 @@ function Content() {
                 <UnderlineRose/>
             </div>
             </section>
-            <div className="relative">
+            <div className="relative md:w-1/2">
             <Image
                 className="md:hidden"
             src='/images/mobile/image-graphic-design.jpg'
+            alt="cherry"
+            width={800}
+            height={1000}    
+                />
+            <Image
+                className="max-md:hidden "
+            src='/images/desktop/image-graphic-design.jpg'
             alt="cherry"
             width={800}
             height={1000}    
@@ -76,10 +98,17 @@ function Content() {
                     </div>
                 </div>
             </div>
-            <div className="relative m-0 p-0">
+            <div className="relative m-0 p-0 md:w-1/2">
             <Image
                 className="md:hidden"
             src='/images/mobile/image-photography.jpg'
+            alt="orange"
+            width={800}
+            height={1000}    
+                />
+            <Image
+                className="max-md:hidden"
+            src='/images/desktop/image-photography.jpg'
             alt="orange"
             width={800}
             height={1000}    
@@ -89,6 +118,7 @@ function Content() {
                     <div className={barlow.className}>
                         <p className="text-xl m-4 p-2">  Increase your credibility by getting the most stunning, high-quality photos that improve your business image.</p>
                     </div>
+                </div>
                 </div>
             </div>
         <Testimonials/>
