d20b32cfb2c127b7e77ef520891311e181f02d5d
Timothy Fawcett <tim.fawcett89@gmail.com>
2023-02-04 16:11:17 +1100

    finalising layouts

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -85,32 +85,60 @@ function Testimonials() {
             </div>
             <div className="grid grid-cols-2 md:grid-cols-4">
                 <Image
-                    className=""
+                    className="md:hidden"
                     src="/images/mobile/image-gallery-milkbottles.jpg"
                     alt=""
                     width={400}
                     height={400}
                 />
                 <Image
-                    className=""
+                    className="max-md:hidden"
+                    src="/images/desktop/image-gallery-milkbottles.jpg"
+                    alt=""
+                    width={800}
+                    height={800}
+                />
+                <Image
+                    className="md:hidden"
                     src="/images/mobile/image-gallery-orange.jpg"
                     alt=""
                     width={400}
                     height={400}
                 />
                 <Image
-                    className=""
+                    className="max-md:hidden"
+                    src="/images/desktop/image-gallery-orange.jpg"
+                    alt=""
+                    width={800}
+                    height={800}
+                />
+                <Image
+                    className="md:hidden"
                     src="/images/mobile/image-gallery-cone.jpg"
                     alt=""
                     width={400}
                     height={400}
                 />
                 <Image
-                    className=""
+                    className="max-md:hidden"
+                    src="/images/desktop/image-gallery-cone.jpg"
+                    alt=""
+                    width={800}
+                    height={800}
+                />
+                <Image
+                    className="md:hidden"
                     src="/images/mobile/image-gallery-sugar-cubes.jpg"
                     alt=""
                     width={400}
                     height={400}
+                />
+                <Image
+                    className="max-md:hidden"
+                    src="/images/desktop/image-gallery-sugarcubes.jpg"
+                    alt=""
+                    width={800}
+                    height={800}
                 />
             </div>
             
--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import Footer from '../components/Footer'
+import Image from 'next/image'
 
 function Contact() {
   return (
     <>
       <Banner />
+      <Image
+            className="h-full-w-full blur-sm -mb-10"
+            src={"/images/desktop/Ai-bg-2.png"}
+            alt={'header image'}
+            width={2000}
+            height={2000}
+      />
       <Footer/>
     </>
   )
