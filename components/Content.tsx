c00f6168f7ecdc0a0429cfc7ccd925a62aa6c4aa
Timothy Fawcett <tim.fawcett89@gmail.com>
2023-02-27 10:40:07 +1100

    small fix

--- components/Content.tsx
+++ components/Content.tsx
@@ -28,7 +28,7 @@ function Content() {
     return (
         <>
             <section className="md:grid md:grid-cols-2 place-items-center">
-            <Image
+            <img
             className="md:hidden"
             src='/images/mobile/image-transform.jpg'
             alt="egg"
@@ -37,7 +37,7 @@ function Content() {
         />
                 <div className="h-96 text-center m-8 md:h-52 md:text-left md:w-80">
     
-            <div className="border-0 text-gray-900 xxs:text-xl xs:text-4xl pt-4 md:text-2xl xl:text-4xl">
+            <div className="text-gray-900 xxs:text-xl xs:text-4xl pt-4 md:text-2xl xl:text-4xl">
             <h3 tabIndex={0} className={fraunces.className}>Transform your <br /> brand</h3>
             </div>
             <p tabIndex={0} className="xxs:text-sm xs:text-base m-7 mx-10 text-gray-500 text-xl md:m-2 md:text-sm md:ml-0 xl:text-lg xl:py-3">  We are a full-service creative agency specializing in helping brands grow fast. 
@@ -58,21 +58,21 @@ function Content() {
                         </div>
     
             </div>
-            <Image
+            <img
             className=" max-md:hidden"
             src='/images/desktop/image-transform.jpg'
             alt="egg"
             width={1000}
             height={1000}
         />
-            <Image
+            <img
             className="md:hidden"
             src='/images/mobile/image-stand-out.jpg'
             alt="cup"
             width={800}
             height={1000}
             />
-            <Image
+            <img
             className=" max-md:hidden"
             src='/images/desktop/image-stand-out.jpg'
             alt="cup"
@@ -100,14 +100,14 @@ function Content() {
                         </div>
             </div>
             <div className="relative ">
-            <Image
+            <img
                 className="md:hidden"
             src='/images/mobile/image-graphic-design.jpg'
             alt="cherry"
             width={800}
             height={1000}    
                 />
-            <Image
+            <img
                 className="max-md:hidden"
             src='/images/desktop/image-graphic-design.jpg'
             alt="cherry"
@@ -122,14 +122,14 @@ function Content() {
                 </div>
             </div>
             <div className="relative m-0 p-0 ">
-            <Image
+            <img
                 className="md:hidden"
             src='/images/mobile/image-photography.jpg'
             alt="orange"
             width={800}
             height={1000}    
                 />
-            <Image
+            <img
                 className="max-md:hidden"
             src='/images/desktop/image-photography.jpg'
             alt="orange"
